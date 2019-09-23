8acf9cad0ae252862ce25607aff1bd1cd89d111d
Sabrina Long <sabrina.long3@gmail.com>
2019-09-22 21:50:11 -0400

    Working on joystick code

--- task1.py
+++ task1.py
@@ -1,53 +1,37 @@
 #!/usr/bin/python3
-import os
 import gpiozero as gp
 import time
 
 # Uses BCM pin numbering
 buttonPin = 4
 switchPin = 24
-joyXpin = 22
-joyYpin = 23
+joyX = 22
+joyY = 23
 
 button = gp.Button(buttonPin)
 switch = gp.Button(switchPin)
-joyX = gp.GPIODevice(joyXpin)
 
 # Global variables
 switchState = 0
 changingMode = False
 modeCount = 1
 
-# Function definitions
-def button_callback():
+def button_callback(channel):
     print("Button was pushed!")
 
-def switch_callback():
-    global switchState
-    global changingMode
 
+def switch_callback(channel):
     switchState = not switchState
-    changingMode = not changingMode
-    if changingMode:
-        print('\n----------')
-        print('CHANGE MODE ON')
-        print('----------\n')
-        print('Current state: ' + str(modeCount))
-    else:
-        print('\n----------')
-        print('CHANGE MODE OFF')
-        print('----------\n')
+    print(switchState)
 
-def inc_mode():
+def inc_mode(channel):
     global modeCount
     modeCount += 1
     if modeCount > 3:
-        modeCount = modeCount%3
+        modeCount = modeCount%3 + 1
     print("Now in state: " + str(modeCount))
 
-# Main loop
 while True:
-    print(joyX.value)
     switch.when_pressed = switch_callback
     switch.when_released = switch_callback
 
